8881daeffbcd0da8f7963eaaca4d6271505ff864
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-04-02 00:31:51 +0200

    Ispravljen import klase Character

--- json-api-caller/src/main/java/rs/ac/bg/fon/np/json_api_caller/main/Main3.java
+++ json-api-caller/src/main/java/rs/ac/bg/fon/np/json_api_caller/main/Main3.java
@@ -2,13 +2,13 @@ package rs.ac.bg.fon.np.json_api_caller.main;
 
 import java.io.FileReader;
 import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import rs.ac.bg.fon.np.json_api_caller.domain.Character;
 
 
 public class Main3 {
