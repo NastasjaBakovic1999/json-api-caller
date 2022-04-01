27541837e7d18f45a047d397e06e3b7ca0553098
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-04-02 00:01:48 +0200

    Dodat Main3 koji cita karaktere upisane u fajl characters.json

---
+++ json-api-caller/src/main/java/rs/ac/bg/fon/np/json_api_caller/main/Main3.java
@@ -0,0 +1,32 @@
+package rs.ac.bg.fon.np.json_api_caller.main;
+
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		try(FileReader in = new FileReader("characters.json")){
+			List<Character> characters;
+			
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			Type type = new TypeToken<LinkedList<Character>>() {}.getType();
+			
+			characters = gson.fromJson(in, type);
+			
+			for(Character ch: characters)
+				System.out.println(ch);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
